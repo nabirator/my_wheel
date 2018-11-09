4ec920edca4864d2123a62fffa8a3306808f7784
Yurii Zhuravlov <jovany@ua.fm>
2018-11-09 07:37:22 +0200

    Use Request class

--- index.php
+++ index.php
@@ -14,6 +14,32 @@ use Symfony\Component\HttpFoundation\Response;
 
 $request = Request::createFromGlobals();
 
+// the URI being requested (e.g. /about) minus any query parameters
+$request->getPathInfo();
+
+// retrieve GET and POST variables respectively
+$request->query->get('foo');
+$request->request->get('bar', 'default value if bar does not exist');
+
+// retrieve SERVER variables
+$request->server->get('HTTP_HOST');
+
+// retrieves an instance of UploadedFile identified by foo
+$request->files->get('foo');
+
+// retrieve a COOKIE value
+$request->cookies->get('PHPSESSID');
+
+// retrieve an HTTP request header, with normalized, lowercase keys
+$request->headers->get('host');
+$request->headers->get('content_type');
+
+$request->getMethod();    // GET, POST, PUT, DELETE, HEAD
+$request->getLanguages(); // an array of languages the client accepts
+
+// simulate a request
+$request = Request::create('/index.php?name=Fabien');
+
 $name = $request->get('name', 'World');
 
 $response = new Response(sprintf('Hello %s', htmlspecialchars($name, ENT_QUOTES, 'UTF-8')));
