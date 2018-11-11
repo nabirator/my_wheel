acf6efe8f00ce208270626a3696305d1a1db81fd
Yurii Zhuravlov <jovany@ua.fm>
2018-11-11 23:18:21 +0200

    Generating absolute url, using context.

--- src/pages/test_route_generator.php
+++ src/pages/test_route_generator.php
@@ -5,3 +5,11 @@ $generator = new Routing\Generator\UrlGenerator($routes, $context);
 
 // outputs /hello/Fabien
 echo $generator->generate('hello', array('name' => 'Fabien'));
+
+echo '<br/>';
+// outputs http://{sitename}/somewhere/hello/Fabien
+echo $generator->generate(
+    'hello',
+    array('name' => 'Fabien'),
+    Routing\Generator\UrlGeneratorInterface::ABSOLUTE_URL
+);
