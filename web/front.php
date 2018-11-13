09dbcb6c06e0e7468ab5ab111532e633e0601eb8
Yurii Zhuravlov <jovany@ua.fm>
2018-11-13 21:47:54 +0200

    Templating. Updating front controller with render_template() function.

--- web/front.php
+++ web/front.php
@@ -6,11 +6,16 @@ use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing;
 
-$request = Request::createFromGlobals();
+function render_template($request)
+{
+    extract($request->attributes->all(), EXTR_SKIP);
+    ob_start();
+    include sprintf(__DIR__.'/../src/pages/%s.php', $_route);
 
-//$routes = new Routing\RouteCollection();
-//$routes->add('hello', new Routing\Route('/hello/{name}', array('name' => 'World')));
-//$routes->add('bye', new Routing\Route('/bye'));
+    return new Response(ob_get_clean());
+}
+
+$request = Request::createFromGlobals();
 $routes = include __DIR__.'/../src/app.php';
 
 $context = new Routing\RequestContext();
@@ -18,11 +23,8 @@ $context->fromRequest($request);
 $matcher = new Routing\Matcher\UrlMatcher($routes, $context);
 
 try {
-    extract($matcher->match($request->getPathInfo()), EXTR_SKIP);
-    ob_start();
-    include sprintf(__DIR__.'/../src/pages/%s.php', $_route);
-
-    $response = new Response(ob_get_clean());
+    $request->attributes->add($matcher->match($request->getPathInfo()));
+    $response = call_user_func('render_template', $request);
 } catch (Routing\Exception\ResourceNotFoundException $exception) {
     $response = new Response('Not Found', 404);
 } catch (Exception $exception) {
